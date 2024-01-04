310edcfeb3ed199ba9873a03ff49b105ee02f048
bhuwan-buda <bhuwanbuda143@gmail.com>
2024-01-04 13:34:02 +0545

    SW added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import "react-color-palette/css";
 import ChessTimer from "./Components/ChessTimer";
+import { useEffect } from "react";
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
@@ -6,6 +6,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { store, persistor } from "./Store/store";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
